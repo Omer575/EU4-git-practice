f49dabd17023ab0428a735a910922fd2471b9887
Omer575 <omer.ulanuluu@hotmail.com>
2021-02-25 19:35:42 +0100

    movie2 branch watching on the phone for 10 min

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World EU4");
         System.out.println("EU4 future SDET learning Git");
+        // movie on the phone 10 min
     }
 }
