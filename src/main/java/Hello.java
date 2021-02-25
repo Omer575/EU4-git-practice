0ca83562fb0d68ed22fbbc56ef346f6eeefd4d92
Omer575 <omer.ulanuluu@hotmail.com>
2021-02-25 19:40:09 +0100

    movie2 branch watching on the phone before going back to living room

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
         System.out.println("EU4 future SDET learning Git");
         // movie on the phone 10 min
         // movie on the phone 20 min
+        // another final 10 min before going back to living room
     }
 }
